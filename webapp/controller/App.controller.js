f82f411ff0d96f4a5d201e431e3000006c1d7584
wvxbs <gabriel.ferreira7854@gmail.com>
2019-02-15 11:58:26 -0200

    a

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -2,14 +2,19 @@ sap.ui.define([
     'sap/ui/core/mvc/Controller',
     'sap/ui/model/json/JSONModel',
     'sap/m/MessageToast',
-    'sap/m/MessageBox'
-], function(Controller, JSONModel, MessageToast, MessageBox) {
+    'sap/m/MessageBox',
+    'sap/ui/model/Sorter',
+    'sap/ui/model/Filter',
+    'sap/ui/model/FilterOperator',
+    'sap/ui/model/FilterType'
+], function(Controller, JSONModel, MessageToast, MessageBox, Sorter, Filter, FilterOperator, FilterType) {
     'use strict'
 
     return Controller.extend('sap.ui.demo.sidebar.controller.App', {
         onInit : function () {
             var JSONData = {
-                busy : false
+                busy : false,
+                order : 0
             }
             var oModel = new JSONModel(oJSONData)
             this.getView().setModel(oModel, 'appView')
@@ -27,6 +32,11 @@ sap.ui.define([
             MessageToast.show(this._getText('refreshSuccessMessage'))
 
         },
+
+        onSearch() : function() {
+          var oView = this.getView(),
+          sValue = oView.byId('searchField').getValue()
+        }
 
         _getText : function (sTextId, aArgs) {
             return this.getOwnerComponent().getModel('i18n').getResourceBundle().getText(sTextId, aArgs)
