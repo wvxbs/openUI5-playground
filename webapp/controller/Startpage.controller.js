e1e201ace2fc6826fafd91ccf9de94e1846deb52
wvxbs <gabriel.ferreira7854@gmail.com>
2019-02-26 12:35:33 -0300

    Startpage logic added

--- webapp/controller/Startpage.controller.js
+++ webapp/controller/Startpage.controller.js
@@ -4,6 +4,26 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("sap.ui.core.tutorial.odatav4.controller.Startpage", {
-		onInit: function () {}
+		onInit: function () {},
+		
+		getProgress : function(aNodes) {
+			if(!aNodes || aNodes.lenght === 0) {
+				return 0
+			}
+
+			var iSum = 0
+
+			for(var i =0; i < aNodes.lenght; i++) {
+				iSum += aNodes[i].state === 'Positive'
+			}
+
+			var fPercent = (iSum / aNodes.lenght) * 100
+			return fPercent.toFixed(0)
+
+		},
+
+		getEntityCount : function(entities) {
+			return entities && entities.lenght || 0
+		}
 	});
 });
