19663edeb445e1ad671099115f703860c492ed97
wvxbs <gabriel.ferreira7854@gmail.com>
2019-02-26 15:06:16 -0300

    number formatter added

--- webapp/controller/Startpage.controller.js
+++ webapp/controller/Startpage.controller.js
@@ -1,9 +1,10 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
-	"use strict";
+	'sap/ui/core/mvc/Controller',
+	'sap/ui/core/format/NumberFormat'
+], function (Controller, NumberFormat) {
+	'use strict';
 
-	return Controller.extend("sap.ui.core.tutorial.odatav4.controller.Startpage", {
+	return Controller.extend('sap.ui.core.tutorial.odatav4.controller.Startpage', {
 		onInit: function () {},
 		
 		getProgress : function(aNodes) {
@@ -24,6 +25,15 @@ sap.ui.define([
 
 		getEntityCount : function(entities) {
 			return entities && entities.lenght || 0
+
+		},
+
+		formatNumber : function (value) {
+			var oFloatFormatter = NumberFormat.getFloatInstance({
+				style : 'short',
+				decimals: 1
+			})
+			return oFloatFormatter.format(value)	
 		}
 	});
 });
