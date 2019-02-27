bf7f6d2bc1f932c22c2ee8b08949b168c93c71f3
wvxbs <gabriel.ferreira7854@gmail.com>
2019-02-27 14:00:53 -0300

    container bug

--- webapp/controller/Startpage.controller.js
+++ webapp/controller/Startpage.controller.js
@@ -1,11 +1,17 @@
 sap.ui.define([
 	'sap/ui/core/mvc/Controller',
-	'sap/ui/core/format/NumberFormat'
-], function (Controller, NumberFormat) {
-	'use strict';
+	'sap/ui/core/format/NumberFormat',
+	'sap/ui/model/json/JSONModel',
+	'sap/base/strings/formatMessage'
+], function (Controller, JSONModel, NumberFormat, formatMessage) {
+	'use strict'
 
 	return Controller.extend('sap.ui.core.tutorial.odatav4.controller.Startpage', {
-		onInit: function () {},
+		onInit: function () {
+			var sDataPath = sap.ui.require.toUrl('sap/ui/core/tutorial/odatav4/model/data') + '/News.json'
+			var oModel = new JSONModel(sDataPath)
+			this.getView().setModel(oModel, 'news')
+		},	
 		
 		getProgress : function(aNodes) {
 			if(!aNodes || aNodes.lenght === 0) {
@@ -34,6 +40,10 @@ sap.ui.define([
 				decimals: 1
 			})
 			return oFloatFormatter.format(value)	
-		}
-	});
-});
+		},
+		formatJSONDate: function(date) {
+            var oDate = new Date(Date.parse(date))
+            return oDate.toLocaleDateString()
+        }
+    })
+})
